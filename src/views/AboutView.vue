ab862393d33862c88338f6b805e110c4809ddcbc
Alexandre <alex100dre.pro@gmail.com>
2024-12-04 18:27:08 +0100

    feat: add vue.js paris logo animation

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,10 +1,31 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="logo-container">
+      <img alt="Vue logo" class="logo" src="@/assets/vjsparis.png" width="500" />
+    </div>
   </div>
 </template>
 
 <style>
+.logo-container {
+  display: inline-block;
+  animation: bounce 4s infinite ease-in-out;
+}
+
+.logo {
+  width: 500px;
+  display: block;
+}
+
+@keyframes bounce {
+  0%, 100% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(-20px);
+  }
+}
+
 @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
